3335f7e8107b17832323e0f6ce60a76f3cf9b207
t91310042 <akiffaskinn@gmail.com>
2020-12-27 14:09:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+digits="0123456789"
+print(digits[0:5:1])
+print(digits[0:5:2])
+print(digits[2::])
+
+
+list= ["one","two","three"]
+print(list.index("two"))
+list.remove("one")
+print(list)
+print(list*2)
+list.insert(0,"ababab")
+print(list)
